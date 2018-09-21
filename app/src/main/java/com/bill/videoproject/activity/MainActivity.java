a96bf6e7ca2212cecd573993d52477cb3ce7b569
Bill <ywbandroid@gmail.com>
2018-09-21 09:05:00 +0800

    代码精简

--- app/src/main/java/com/bill/videoproject/activity/MainActivity.java
+++ app/src/main/java/com/bill/videoproject/activity/MainActivity.java
@@ -1,57 +1,22 @@
 package com.bill.videoproject.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
 
 import com.bill.videoproject.R;
-import com.bill.videoproject.widget.media.IjkVideoView;
-
-import tv.danmaku.ijk.media.player.IMediaPlayer;
-import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
-
-    private boolean mBackPressed;
-    private IjkVideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mVideoView = findViewById(R.id.video_view);
-
-        IjkMediaPlayer.loadLibrariesOnce(null);
-        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
-//        mVideoView.setVideoPath("http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f30.mp4");
-//        mVideoView.setVideoPath("http://rmrbtest-image.peopleapp.com/upload/video/201809/153717629785fa37050329756a.mp4");
-        mVideoView.setVideoPath("http://rmrbtest-image.peopleapp.com/upload/video/201809/1537349021125fcfb438615c1b.mp4");
-        mVideoView.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(IMediaPlayer mp) {
-                Log.e("Bill", "prepared");
-                mVideoView.start();
-            }
-        });
-
     }
 
-    @Override
-    public void onBackPressed() {
-        mBackPressed = true;
-        super.onBackPressed();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mBackPressed || !mVideoView.isBackgroundPlayEnabled()) {
-            mVideoView.stopPlayback();
-            mVideoView.release(true);
-            mVideoView.stopBackgroundPlay();
-        } else {
-            mVideoView.enterBackground();
-        }
-        IjkMediaPlayer.native_profileEnd();
+    public void handlePlayer(View view) {
+        Intent intent = new Intent(this, VideoActivity.class);
+        startActivity(intent);
     }
 }
