6ab9e3ff91daba6a0fc6ac75e9e2f3222a59478d
Bill <ywbandroid@gmail.com>
2018-10-16 16:13:12 +0800

    添加像素设置

--- app/src/main/java/com/bill/videoproject/activity/VideoSettingActivity.java
+++ app/src/main/java/com/bill/videoproject/activity/VideoSettingActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioGroup;
 
 import com.bill.videoproject.R;
@@ -15,25 +16,45 @@ import com.bill.videoproject.setting.ScaleType;
 
 public class VideoSettingActivity extends AppCompatActivity {
 
+    private CheckBox backgroundBackPlay;
     private RadioGroup mPlayerTypeGroup;
-    private RadioGroup mRenderTypeGroup;
-    private RadioGroup mScaleTypeGroup;
+
+    private CheckBox useMediaCodec;
+    private CheckBox useMediaCodecAutoRotate;
+    private CheckBox mediaCodecHandleResolutionChange;
     private RadioGroup mPixelFormatTypeGroup;
+    private RadioGroup mScaleTypeGroup;
+
+    private CheckBox usingOpenslEs;
+
+    private RadioGroup mRenderTypeGroup;
+    private CheckBox enableDetachedSurfaceTexture;
+
+    private CheckBox usingMediaDataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_setting);
 
+        backgroundBackPlay = findViewById(R.id.background_play);
         mPlayerTypeGroup = findViewById(R.id.player_type);
-        mRenderTypeGroup = findViewById(R.id.render_type);
-        mScaleTypeGroup = findViewById(R.id.scale_type);
+        useMediaCodec = findViewById(R.id.use_media_codec);
+        useMediaCodecAutoRotate = findViewById(R.id.use_media_codec_auto_rotate);
+        mediaCodecHandleResolutionChange = findViewById(R.id.media_codec_handle_resolution_change);
         mPixelFormatTypeGroup = findViewById(R.id.pixel_format_type);
+        mScaleTypeGroup = findViewById(R.id.scale_type);
+        usingOpenslEs = findViewById(R.id.using_opensl_es);
+        mRenderTypeGroup = findViewById(R.id.render_type);
+        enableDetachedSurfaceTexture = findViewById(R.id.enable_detached_surface_texture);
+        usingMediaDataSource = findViewById(R.id.using_mediadatasource);
+
 
     }
 
 
     private void goVideoAct() {
+        PlayerConfig.getInstance().setEnableBackgroundPlay(backgroundBackPlay.isChecked());
         switch (mPlayerTypeGroup.getCheckedRadioButtonId()) {
             case R.id.ijk_player:
                 PlayerConfig.getInstance().setPlayer(PlayerType.PV_PLAYER__IjkMediaPlayer);
@@ -45,15 +66,25 @@ public class VideoSettingActivity extends AppCompatActivity {
                 PlayerConfig.getInstance().setPlayer(PlayerType.PV_PLAYER__IjkExoMediaPlayer);
                 break;
         }
-        switch (mRenderTypeGroup.getCheckedRadioButtonId()) {
-            case R.id.surface_view:
-                PlayerConfig.getInstance().setRender(RenderType.RENDER_SURFACE_VIEW);
+
+        PlayerConfig.getInstance().setUsingMediaCodec(useMediaCodec.isChecked());
+        PlayerConfig.getInstance().setUsingMediaCodecAutoRotate(useMediaCodecAutoRotate.isChecked());
+        PlayerConfig.getInstance().setMediaCodecHandleResolutionChange(mediaCodecHandleResolutionChange.isChecked());
+        switch (mPixelFormatTypeGroup.getCheckedRadioButtonId()) {
+            case R.id.rgb_565:
+                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.RGB_565);
                 break;
-            case R.id.texture_view:
-                PlayerConfig.getInstance().setRender(RenderType.RENDER_TEXTURE_VIEW);
+            case R.id.rgb_888:
+                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.RGB_888);
                 break;
-            case R.id.no_view:
-                PlayerConfig.getInstance().setRender(RenderType.RENDER_NONE_VIEW);
+            case R.id.rgbx_8888:
+                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.RGBX_8888);
+                break;
+            case R.id.yv12:
+                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.YV12);
+                break;
+            case R.id.opengl_es2:
+                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.OpenGL_ES2);
                 break;
         }
         switch (mScaleTypeGroup.getCheckedRadioButtonId()) {
@@ -76,23 +107,24 @@ public class VideoSettingActivity extends AppCompatActivity {
                 PlayerConfig.getInstance().setScale(ScaleType.AR_4_3_FIT_PARENT);
                 break;
         }
-        switch (mPixelFormatTypeGroup.getCheckedRadioButtonId()) {
-            case R.id.rgb_565:
-                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.RGB_565);
+
+        PlayerConfig.getInstance().setUsingOpenSLES(usingOpenslEs.isChecked());
+
+        switch (mRenderTypeGroup.getCheckedRadioButtonId()) {
+            case R.id.surface_view:
+                PlayerConfig.getInstance().setRender(RenderType.RENDER_SURFACE_VIEW);
                 break;
-            case R.id.rgb_888:
-                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.RGB_888);
+            case R.id.texture_view:
+                PlayerConfig.getInstance().setRender(RenderType.RENDER_TEXTURE_VIEW);
                 break;
-            case R.id.rgbx_8888:
-                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.RGBX_8888);
-                break;
-            case R.id.yv12:
-                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.YV12);
-                break;
-            case R.id.opengl_es2:
-                PlayerConfig.getInstance().setPixelFormat(PixelFormatType.OpenGL_ES2);
+            case R.id.no_view:
+                PlayerConfig.getInstance().setRender(RenderType.RENDER_NONE_VIEW);
                 break;
         }
+        PlayerConfig.getInstance().setEnableDetachedSurfaceTextureView(enableDetachedSurfaceTexture.isChecked());
+
+        PlayerConfig.getInstance().setUsingMediaDataSource(usingMediaDataSource.isChecked());
+
 
         Intent intent = new Intent(this, VideoActivity.class);
         startActivity(intent);
